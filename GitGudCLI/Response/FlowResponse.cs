3452f53a37c9af0339ef9426a80d56ec020429bc
Helton Reis <heltonkreis@gmail.com>
2021-02-04 23:14:21 -0300

    [feature] Add responses structs

---
+++ GitGudCLI/Response/FlowResponse.cs
@@ -0,0 +1,20 @@
+﻿using GitGudCLI.Structure;
+
+namespace GitGudCLI.Response
+{
+    public struct FlowResponse
+    {
+        public bool Success { get; init; }
+        public EnumFlowResponse Response { get; init; }
+        public EnumGitResponse GitReponse { get; init; }
+        public string Message { get; init; }
+
+        public FlowResponse(bool success, EnumFlowResponse response, EnumGitResponse gitResponse, string message)
+        {
+            Success = success;
+            Response = response;
+            GitReponse = gitResponse;
+            Message = message;
+        }
+    }
+}
---
+++ GitGudCLI/Response/GitReponse.cs
@@ -0,0 +1,18 @@
+﻿using GitGudCLI.Structure;
+
+namespace GitGudCLI.Response
+{
+    public struct GitResponse
+    {
+        public bool Success { get; init; }
+        public EnumGitResponse Response { get; init; }
+        public string Message { get; init; }
+
+        public GitResponse(bool success, EnumGitResponse response, string message)
+        {
+            Success = success;
+            Response = response;
+            Message = message;
+        }
+    }
+}
