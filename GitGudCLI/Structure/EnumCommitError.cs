9a3c9082958681684a00e55ae8eb98964d4b8b64
Helton Reis <heltonkreis@gmail.com>
2021-02-04 23:15:34 -0300

    [feature] Add commit errors and warning enums

---
+++ GitGudCLI/Structure/EnumCommitError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitGudCLI.Structure
+{
+    [Flags]
+    public enum EnumCommitError : byte
+    {
+        NONE = 0,
+        NO_TAG = 1,
+        NO_SUBJECT = 2,
+        FLAG = 4,
+        CLOSED_ISSUES = 8,
+        SEE_ALSO = 16,
+        INVALID_TAG = 32,
+        INVALID_FLAG = 64
+    }
+}
---
+++ GitGudCLI/Structure/EnumCommitWarning.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitGudCLI.Structure
+{
+    [Flags]
+    public enum EnumCommitWarning : byte
+    {
+        NONE = 0,
+        SUBJECT_TOO_LONG = 1,
+        BREAKING_CHANGE_IS_NOT_THE_FIRST_FLAG = 2
+    }
+}
