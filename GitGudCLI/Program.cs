44bf03e55458ac1708869d8b5f50715e4078f249
Helton Reis <heltonkreis@gmail.com>
2021-02-04 23:09:37 -0300

    [misc] Initial commit

---
+++ GitGudCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitGudCLI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
