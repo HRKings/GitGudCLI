6799ca68961227ffb9b22a05b8d57dd48a9c69e0
Helton Reis <heltonkreis@gmail.com>
2021-02-04 23:13:18 -0300

    [feature] Add responses enums

---
+++ GitGudCLI/Structure/EnumFlowResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace GitGudCLI.Structure
+{
+    public enum EnumFlowResponse : byte
+    {
+        NONE = 0,
+        GENERIC_ERROR = 1,
+        GIT_ERROR = 2,
+        STABLE_ALREADY_EXISTS = 3,
+        MASTER_ALREADY_EXISTS = 4,
+        INVALID_WORKING_TYPE = 5,
+        CANNOT_COMPLETE_PERMANENT_BRANCHES = 6,
+        REMOTE_BRANCHES_NEED_PULLREQUEST = 7
+    }
+}
---
+++ GitGudCLI/Structure/EnumGitResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace GitGudCLI.Structure
+{
+    public enum EnumGitResponse : byte
+    {
+        NONE = 0,
+        GENERIC_ERROR = 1,
+        FATAL_ERROR = 2,
+        BRANCH_ALREADY_EXISTS = 3,
+        BRANCH_DOESNT_EXISTS = 4,
+        REPOSITORY_HAS_NO_ORIGIN = 5
+    }
+}
