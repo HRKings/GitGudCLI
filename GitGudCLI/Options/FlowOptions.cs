e68d146bdd5b80435851071257c1a6679e355afc
Helton Reis <heltonkreis@gmail.com>
2021-02-17 23:01:33 -0300

    [feature] Add flow options parsing

---
+++ GitGudCLI/Options/FlowOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace GitGudCLI.Options
+{
+	[Verb("flow", HelpText = "GitGud Flow actions")]
+	public class FlowOptions
+	{
+		[Value(0, Required = true, HelpText = "The Flow action to execute (fullinit, init, start, publish, complete)")] 
+		public string Action { get; set; }
+		
+		[Value(1, HelpText = "The branch name to use in the action")] 
+		public string BranchName { get; set; }
+	}
+}
--- GitGudCLI/Program.cs
+++ GitGudCLI/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using CommandLine;
 using ConsoleHelper;
+using GitGudCLI.Commands;
 using GitGudCLI.Options;
+using GitGudCLI.Response;
 using GitGudCLI.Utils;
 
 namespace GitGudCLI
@@ -14,44 +16,9 @@ namespace GitGudCLI
 		{
 			_gitHelper = new GitHelper();
 			
-			Parser.Default.ParseArguments<CommitOptions>(args)
-				.WithParsed<CommitOptions>(options => CommitCommands.Run(options, _gitHelper));
-
-			if (args.Length == 0)
-			{
-				Console.WriteLine("HELP_TEXT");
-				return 1;
-			}
-
-			switch (args[0])
-			{
-				case "changelog":
-				case "chlg":
-
-					break;
-
-				case "flow":
-					switch (args.Length)
-					{
-						case 2:
-							CLIMethods.Flow(args[1], string.Empty, _gitHelper);
-							break;
-
-						case 3:
-							CLIMethods.Flow(args[1], args[2], _gitHelper);
-							break;
-
-						default:
-							ColorConsole.WriteError("Not enough arguments.");
-							break;
-					}
-
-					break;
-
-				default:
-					Console.WriteLine($"Argument \"{args[0]}\" is not valid.");
-					break;
-			}
+			Parser.Default.ParseArguments<CommitOptions, FlowOptions>(args)
+				.WithParsed<CommitOptions>(options => CommitCommands.Run(options, _gitHelper))
+				.WithParsed<FlowOptions>(options => FlowCommands.Run(options, _gitHelper));
 
 			return 0;
 		}
