ef5a3c1a0822cd6533ac02d0942af53d2800fc1b
Helton Reis <heltonkreis@gmail.com>
2021-06-08 01:43:03 -0300

    [feature] dasd

---
+++ compileDevLocal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -o ./compiled ; mv ./compiled/GitGudCLI ~/.bin/gitgud ; chmod +x ~/.bin/gitgud
